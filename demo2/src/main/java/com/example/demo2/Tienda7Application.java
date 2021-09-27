8592e2612315ff775df41ecb6a6cac4c6adf0872
USER <USER@DESKTOP-I9N5CM5>
2021-09-26 22:45:05 -0500

    Esta es la primera subida de archivos al backend

---
+++ demo2/src/main/java/com/example/demo2/Tienda7Application.java
@@ -0,0 +1,13 @@
+package com.example.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tienda7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tienda7Application.class, args);
+	}
+
+}
